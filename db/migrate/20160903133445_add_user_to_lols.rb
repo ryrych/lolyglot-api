688a71e734e2901e9c2299f3c7e61fa66942b65e
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-09-03 15:35:37 +0200

    Add user_id to lols

---
+++ db/migrate/20160903133445_add_user_to_lols.rb
@@ -0,0 +1,5 @@
+class AddUserToLols < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :lols, :user, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903133318) do
+ActiveRecord::Schema.define(version: 20160903133445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160903133318) do
     t.string   "term"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_lols_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 20160903133318) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "lols", "users"
 end
