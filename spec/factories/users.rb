4972e31b293f903172fb05b6f12b1fb1535b98e7
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-09-03 18:42:10 +0200

    Add avatar_url to users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user-#{n}@lolyglot.dev" }
-    name { FFaker::Name.name }
-    uid  { FFaker::Guid.guid }
+    avatar_url { Faker::Placeholdit.image("320x320", 'jpg', 'ffffff', '000', name) }
+    name       { FFaker::Name.name }
+    uid        { FFaker::Guid.guid }
     provider 'google'
   end
 end
