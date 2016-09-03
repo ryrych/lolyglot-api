7de27d94058a1eec77c0e75d39f226983d33c86e
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-09-03 18:40:33 +0200

    Add avatar_url to users - migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903163636) do
+ActiveRecord::Schema.define(version: 20160903163946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20160903163636) do
     t.datetime "updated_at", null: false
     t.string   "uid"
     t.string   "provider"
+    t.string   "avatar_url"
   end
 
   add_foreign_key "lols", "users"
