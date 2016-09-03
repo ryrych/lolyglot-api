66231fb71671474cfbb023bea559b4fd2985985d
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-09-03 16:48:25 +0200

    Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  syme = FactoryGirl.create(:user, name: 'Syme', email: 'syme@evaporated.oc')
+  FactoryGirl.create_list(:lol, 10, user: syme)
+end
