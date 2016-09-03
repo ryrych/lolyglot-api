81f590cc64e14c6cb9266eaa1f2de00b7c293faf
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-09-03 15:33:59 +0200

    Add users

---
+++ db/migrate/20160903133318_add_users.rb
@@ -0,0 +1,10 @@
+class AddUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
