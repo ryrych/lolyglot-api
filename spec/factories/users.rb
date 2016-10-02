2d7f31a8023184ef55678b536f299bc20e608ca8
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-10-02 15:52:05 +0200

    Faker => FFaker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user-#{n}@lolyglot.dev" }
-    avatar_url { Faker::Placeholdit.image("320x320", 'jpg', 'ffffff', '000', name) }
+    avatar_url { FFaker::Placeholdit.image("320x320", 'jpg', 'ffffff', '000', name) }
     name       { FFaker::Name.name }
     uid        { FFaker::Guid.guid }
     provider 'google'
